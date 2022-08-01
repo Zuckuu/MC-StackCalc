f0984134b9f1a346238b60c09802caa5a8fba35c
Zuckuu <entbit12@gmail.com>
2022-08-01 16:30:09 -0500

    fixing up

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function calculate() {
   const value = document.getElementById("input").value;
-
   const stack = Math.floor(value / 64);
   const remaining = value % 64;
-  console.log(stack );
-  console.log(remaining);
+  const result = document.getElementById("result");
+
+  result.innerHTML = 'Stack: ' + stack + " and " + remaining + ' items';
 }
